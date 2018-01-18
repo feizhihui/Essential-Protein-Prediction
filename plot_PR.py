ffed34f2bce6cecaf7c669dd8a7dd596f7d19bfc
feizhihui <helloworld@csu.edu.cn>
2018-01-18 20:30:56 +0800

    add plot module

---
+++ plot_PR.py
@@ -0,0 +1,41 @@
+# encoding=utf-8
+from sklearn.metrics import precision_recall_curve, auc
+import matplotlib.pyplot as plt
+import pickle
+import numpy as np
+
+image_list = ['multi_source.pkl',
+              'total6.pkl',
+              'degree_centrality.pkl',
+              'closeness_centrality.pkl',
+              'eigenvector_centrality.pkl',
+              'betweenness_centrality.pkl',
+              'harmonic_centrality.pkl',
+              'load_centrality.pkl']
+
+colors = ['green', 'red', 'cyan', 'magenta', 'lightgray', 'black', 'coral', 'blue']
+legends = ['Doc2Vec',
+           'Totals',
+           'Degree Centrality',
+           'Closeness Centrality',
+           'Eigenvector Centrality',
+           'Betweenness Centrality',
+           'Harmonic Centrality',
+           'Load Centrality']
+
+mean_tpr = 0
+mean_fpr = np.linspace(0, 1, 100)
+plt.title('Precision/Recall Curve')  # give plot a title
+plt.xlabel('Recall')  # make axis labels
+plt.ylabel('Precision')
+for i, filename in enumerate(image_list):
+    with open('plot_cache/' + filename, 'rb') as file:
+        y_score, y_label = pickle.load(file)
+        precision, recall, _ = precision_recall_curve(y_label, y_score)
+        aupr = auc(recall, precision)
+        plt.plot(recall, precision, 'k--', color=colors[i], label='%s AUPR:%.3f' % (legends[i], aupr), lw=2)
+
+plt.legend(loc="lower right", fontsize='small')  # after plt.plot
+plt.show()
+
+plt.savefig('PR.jpg')
---
+++ plot_ROC.py
@@ -0,0 +1,41 @@
+# encoding=utf-8
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
+import pickle
+import numpy as np
+
+image_list = ['multi_source.pkl',
+              'total6.pkl',
+              'degree_centrality.pkl',
+              'closeness_centrality.pkl',
+              'eigenvector_centrality.pkl',
+              'betweenness_centrality.pkl',
+              'harmonic_centrality.pkl',
+              'load_centrality.pkl']
+
+colors = ['green', 'red', 'cyan', 'magenta', 'lightgray', 'black', 'coral', 'blue']
+legends = ['Doc2Vec',
+           'Totals',
+           'Degree Centrality',
+           'Closeness Centrality',
+           'Eigenvector Centrality',
+           'Betweenness Centrality',
+           'Harmonic Centrality',
+           'Load Centrality']
+
+mean_tpr = 0
+mean_fpr = np.linspace(0, 1, 100)
+plt.title('ROC')
+
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+for i, filename in enumerate(image_list):
+    with open('plot_cache/' + filename, 'rb') as file:
+        y_pred, y_label = pickle.load(file)
+        fpr, tpr, thresholds = roc_curve(y_label, y_pred)
+        roc_auc = auc(fpr, tpr)
+        plt.plot(fpr, tpr, 'k--', color=colors[i], label='%s AUC:%.3f' % (legends[i], roc_auc), lw=2)
+
+plt.legend(loc="lower right", fontsize='medium')  # after plt.plot
+plt.show()
+plt.savefig('ROC.jpg')
